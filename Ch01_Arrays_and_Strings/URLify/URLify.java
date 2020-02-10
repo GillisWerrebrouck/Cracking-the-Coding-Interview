7f2ae54ffff49d725bd73a7a9ccad19ad8d0d222
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-02-10 15:07:51 +0100

    Add algorithm to URLify a string

---
+++ Ch01_Arrays_and_Strings/URLify/URLify.java
@@ -0,0 +1,53 @@
+/**
+ * Algorithm to URLify a string.
+ * Spaces become "%20".
+ * @author Gillis Werrebrouck
+ */
+
+public class URLify {
+    /**
+     * @param args: 0: string to URLify, 1: true length
+     */
+    public static void main(String args[]) {
+        if (args.length != 2) {
+            System.out.println("Two parameters expected");
+            return;
+        }
+
+        if (args[0].length() < Integer.parseInt(args[1])) {
+            System.out.println("True length can't be longer than length of string");
+            return;
+        }
+        
+        String URLified = getUrl(args[0].toCharArray(), Integer.parseInt(args[1]));
+        System.out.println("\"" + args[0] + "\" (true length: " + args[1] + ") --- URLified ---> \"" + URLified + "\"");
+    }
+
+    private static String getUrl(char[] url, Integer trueLength) {
+        Integer spaces = 0;
+        for(Integer index = 0; index < trueLength; index++) {
+            if(url[index] == ' ') {
+                spaces++;
+            }
+        }
+
+        Integer newLength = spaces*2 + trueLength;
+        char[] newUrl = new char[newLength];
+        for(Integer index = 0; index < trueLength; index++) {
+            newUrl[index] = url[index];
+        }
+
+        Integer newUrlPointer = newLength - 1;
+        for(Integer index = trueLength-1; index >= 0; index--) {
+            if(url[index] == ' ') {
+                newUrl[newUrlPointer--] = '0';
+                newUrl[newUrlPointer--] = '2';
+                newUrl[newUrlPointer--] = '%';
+            } else {
+                newUrl[newUrlPointer--] = url[index];
+            }
+        }
+
+        return new String(newUrl);
+    }
+}
