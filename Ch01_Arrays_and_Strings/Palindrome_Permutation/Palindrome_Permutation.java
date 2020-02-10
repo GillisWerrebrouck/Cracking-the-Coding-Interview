c4f28cad7e2985994fe2d5e7a050c41f8561b34e
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-02-10 16:36:37 +0100

    Add algorithm to check if a string is a permutation of a palindrome

---
+++ Ch01_Arrays_and_Strings/Palindrome_Permutation/Palindrome_Permutation.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+/**
+ * Algorithm to check if a string is a permutation of a palindrome.
+ * @author Gillis Werrebrouck
+ */
+
+public class Palindrome_Permutation {
+    public static void main(String args[]) {
+        IsPalindromeOfPermutation(args[0]);
+        System.out.println("The strings \"" + args[0] + "\" is" + (IsPalindromeOfPermutation(args[0]) ? "" : "n't") + " a permutation of a palindrome.");
+    }
+
+    private static String sort(String str) {
+        char sorted[] = str.toCharArray();
+        Arrays.sort(sorted);
+        return new String(sorted);
+    }
+
+    private static boolean IsPalindromeOfPermutation(String str) {
+        String sorted = sort(str.toLowerCase());
+        
+        // boolean to know if an odd count of a char has occured or not, this can at most happen once in a palindrome
+        boolean canHaveOddCharCount = true;
+
+        for(Integer index = 0; index < sorted.length(); index++) {
+            if(sorted.charAt(index) == ' ') continue;
+
+            Integer countChar = 1;
+            Integer innerIndex = index+1;
+
+            while(sorted.length() > innerIndex && sorted.charAt(index) == sorted.charAt(innerIndex)) {
+                countChar++;
+                innerIndex++;
+            }
+
+            if(countChar % 2 != 0) {
+                if(!canHaveOddCharCount) {
+                    return false;
+                }
+                canHaveOddCharCount = false;
+            }
+
+            index = innerIndex-1;
+        }
+
+        return true;
+    }
+}
