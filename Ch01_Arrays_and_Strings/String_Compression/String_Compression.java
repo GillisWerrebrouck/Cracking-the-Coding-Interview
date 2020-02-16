f9c64d6c93dc565d3eb932fcdf759d8de83e0862
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-02-16 15:46:09 +0100

    Add algorithm to compress strings

---
+++ Ch01_Arrays_and_Strings/String_Compression/String_Compression.java
@@ -0,0 +1,51 @@
+/**
+ * Algorithm to compress a string using the counts of repeated characters.
+ * @author Gillis Werrebrouck
+ */
+
+public class String_Compression {
+    public static void main(String args[]) {
+        if (args.length != 1) {
+            System.out.println("One parameter expected");
+            return;
+        }
+
+        System.out.println(Compress(args[0]));
+    }
+
+    private static Integer countCompressionLength(String original) {
+        Integer compressionCount = 0;
+
+        for(Integer index = 0; index < original.length(); index++) {
+            Integer innerIndex = index+1;
+            while(original.length() > innerIndex && original.charAt(index) == original.charAt(innerIndex)) {
+                innerIndex++;
+            }
+            compressionCount += 2;
+            index=innerIndex-1;
+        }
+
+        return compressionCount;
+    }
+
+    private static String Compress(String original) {
+        Integer compressionLength = countCompressionLength(original);
+        if(compressionLength >= original.length()) {
+            return original;
+        }
+
+        StringBuilder compressed = new StringBuilder(compressionLength);
+
+        for(Integer index = 0; index < original.length(); index++) {
+            Integer innerIndex = index+1;
+            while(original.length() > innerIndex && original.charAt(index) == original.charAt(innerIndex)) {
+                innerIndex++;
+            }
+            compressed.append(original.charAt(index));
+            compressed.append(innerIndex - index);
+            index=innerIndex-1;
+        }
+
+        return compressed.toString();
+    }
+}
