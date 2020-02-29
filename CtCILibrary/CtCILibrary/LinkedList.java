29b55c2913226dbe086bd8111af48cfdd9047a4c
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-02-29 15:26:20 +0100

    Add algorithm to detect if a linked list has a loop

--- CtCILibrary/CtCILibrary/LinkedList.java
+++ CtCILibrary/CtCILibrary/LinkedList.java
@@ -8,10 +8,41 @@ public class LinkedList {
         this.node = n;
         this.setLength();
     }
+
+    public LinkedListNode detectLoop() {
+        if(this.node == null) {
+            return null;
+        }
+
+        LinkedListNode pointer = this.node;
+        LinkedListNode runner = this.node;
+
+        // determine where the pointer and runner collide if the pointer moves one node and the runner moves two nodes each iteration
+        do {
+            if(runner.next == null || runner.next.next == null) {
+                return null;
+            }
+            pointer = pointer.next;
+            runner = runner.next.next;
+        } while(pointer != runner);
+
+        // the colliding node and the start node of the list are now the same amount of nodes away from the start of the loop
+        LinkedListNode collision = pointer;
+        pointer = this.node;
+
+        while(pointer != collision) {
+            pointer = pointer.next;
+            collision = collision.next;
+        }
+        
+        return pointer;
+    }
     
     public void print() {
         LinkedListNode node = this.node;
-        while(node != null) {
+        LinkedListNode endNode = this.detectLoop();
+        
+        while(node != endNode) {
             System.out.print(node.data + (node.next != null ? " -> " : "\n"));
             node = node.next;
         }
@@ -25,6 +56,11 @@ public class LinkedList {
     }
 
     public void appendToTail(LinkedListNode node) {
+        // this method does nothing if there is a loop in the linked list
+        if(this.detectLoop() != null) {
+            return;
+        }
+        
         if(this.node != null) {
             this.node.appendToTail(node);
             this.setLength();
@@ -42,8 +78,9 @@ public class LinkedList {
     public void setLength() {
         this.length = 0;
         LinkedListNode node = this.node;
+        LinkedListNode endNode = this.detectLoop();
 
-        while(node != null) {
+        while(node != endNode) {
             this.length++;
             node = node.next;
         }
