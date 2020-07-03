130dc45614882297602972ad362f0464cc1eb63b
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-07-03 19:34:20 +0200

    Add custom binary tree implementation with a getRandomNode() method

--- CtCILibrary/CtCILibrary/TreeNode.java
+++ CtCILibrary/CtCILibrary/TreeNode.java
@@ -1,6 +1,7 @@
 package CtCILibrary;
 
 import java.util.Queue;
+import java.util.Random;
 import java.util.LinkedList;
 
 public class TreeNode {
@@ -8,18 +9,56 @@ public class TreeNode {
     public TreeNode left = null;
     public TreeNode right = null;
     public TreeNode parent = null;
+    public int size = 0;
 
     public TreeNode(int value) {
         this.value = value;
+        this.size = 1;
     }
 
     public TreeNode(int value, TreeNode parent) {
         this.value = value;
         this.parent = parent;
+        this.size = 1;
     }
 
     public void print() {
         print(0);
+    }
+
+    private TreeNode getNthNode(int N) {
+        int leftSize = (left == null ? 0 : left.size);
+
+        if(N == leftSize) {
+            return this;
+        } else if(N < leftSize) {
+            return left.getRandomNode();
+        } else {
+            return right.getRandomNode();
+        }
+    }
+
+    public TreeNode getRandomNode() {
+        Random rnd = new Random();
+        int rndValue = rnd.nextInt(size);
+
+        return getNthNode(rndValue);
+    }
+
+    public void insert(int value) {
+        if(value <= this .value) {
+            if(left == null)
+                left = new TreeNode(value, this);
+            else
+                left.insert(value);
+        } else {
+            if(right == null)
+                right = new TreeNode(value, this);
+            else
+                right.insert(value);
+        }
+
+        size++;
     }
 
     private void print(int index) {
