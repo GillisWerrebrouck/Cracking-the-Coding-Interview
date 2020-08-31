f3a4a018af4486e63157a1d3ebc7220028a18ef7
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-08-31 22:11:40 +0200

    Add sql query to get all buildings and the amount of open requests per building

---
+++ Ch14_Database/Open_Requests/Open_Requests.sql
@@ -0,0 +1,5 @@
+SELECT Apartments.BuildingID, ISNULL(count(*), 0) as 'Count'
+FROM Apartments
+JOIN Requests ON Requests.AptID = Apartments.AptID
+WHERE Requests.Status = 'Open'
+GROUP BY Apartments.BuildingID
