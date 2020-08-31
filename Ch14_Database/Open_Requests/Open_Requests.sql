ebb8e4b3073c5ec66892f8489a6577b9f4b8f60c
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-08-31 22:18:00 +0200

    Add semicolon

--- Ch14_Database/Open_Requests/Open_Requests.sql
+++ Ch14_Database/Open_Requests/Open_Requests.sql
@@ -2,4 +2,4 @@ SELECT Apartments.BuildingID, ISNULL(count(*), 0) as 'Count'
 FROM Apartments
 JOIN Requests ON Requests.AptID = Apartments.AptID
 WHERE Requests.Status = 'Open'
-GROUP BY Apartments.BuildingID
+GROUP BY Apartments.BuildingID;
